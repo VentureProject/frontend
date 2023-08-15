583255badefd9bfd781ad3b15b92164be92a1d43
juwon5272 <jackson5272@naver.com>
2023-08-15 11:49:23 +0900

    feat : receive product data

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -4,7 +4,7 @@ import ModalImgUpload from "./ModalImgUpload";
 import X from "../images/X.png";
 import xOver from "../images/XOver.png";
 
-function ReviewModal({ setModalOpen, id, title, content, writer }) {
+function ReviewModal({ setModalOpen, modalData }) {
   const [xImg, setXImg] = useState(false);
   const closeModal = () => {
     setModalOpen(false);
@@ -12,15 +12,43 @@ function ReviewModal({ setModalOpen, id, title, content, writer }) {
 
   return (
     <Wrapper>
-      <XButton
-        onMouseOver={() => setXImg(true)}
-        onMouseOut={() => setXImg(false)}
-        src={xImg ? xOver : X}
-        onClick={closeModal}
-      />
-      <Box>포토후기 작성하기</Box>
-      <Box>aaa</Box>
-      <ModalImgUpload />
+      <Top>
+        <XButton
+          onMouseOver={() => setXImg(true)}
+          onMouseOut={() => setXImg(false)}
+          src={xImg ? xOver : X}
+          onClick={closeModal}
+        />
+      </Top>
+      <Box> 포토후기 작성하기</Box>
+      {modalData && (
+        <Box height="auto">
+          <DetailWrapper width="20%">
+            {/* <PhotoWrapper src={ExProduct}></PhotoWrapper> */}
+          </DetailWrapper>
+          <DetailWrapper width="40%">
+            <ProductDetail>{modalData.brandName}</ProductDetail>
+            <ProductDetail>{modalData.productName}</ProductDetail>
+            <ProductDetail marginbottom="0px" color="#9e9e9e" fontSize="20px">
+              사이즈 : {modalData.option}
+            </ProductDetail>
+            <ProductDetail color="#9e9e9e" fontSize="20px">
+              가격 : {modalData.price}원
+            </ProductDetail>
+          </DetailWrapper>
+          {/* <DetailWrapper width="20%">
+            <ProductDetail fontSize="24px">{modalData.date}</ProductDetail>
+          </DetailWrapper> */}
+        </Box>
+      )}
+      {/* <Box>{modalData.brandName}</Box> */}
+      <Box backgroundColor="none" height="240px">
+        <ModalImgUpload id="imgUpload" />
+        <TextReview type="text" placeholder="텍스트 리뷰를 작성하여 주세요" />
+      </Box>
+      <Box onClick={closeModal} justifyContent="center">
+        제출하기
+      </Box>
     </Wrapper>
   );
 }
@@ -29,25 +57,74 @@ export default ReviewModal;
 
 const Wrapper = styled.div`
   position: absolute;
+  max-width: 800px;
   width: 70%;
+  height: auto;
   border-radius: 5px;
   background-color: black;
-  top: 160px;
+  top: 100px;
   left: 15%;
+`;
+
+const Top = styled.div`
+  background-color: #555555;
+  border-radius: 5px 5px 0 0;
 `;
 
 const XButton = styled.img`
   width: 15px;
   height: 15px;
-  margin: 0px 10px;
+  margin: 0px 15px;
 `;
 
 const Box = styled.div`
-  background-color: white;
-  margin: 5px;
+  // background-color: white;
+  background-color: ${(props) => props.backgroundColor || "white"};
+  margin: 8px;
   border-radius: 5px;
-  height: 55px;
+  // height: 55px;
+  height: ${(props) => props.height || "55px"};
   font-size: 24px;
+  display: flex;
+  justify-content: ${(props) => props.justifyContent || "left"};
+  align-items: center;
 `;
 
 const ImgUpload = styled.div``;
+
+const TextReview = styled.textarea`
+  width: 470px;
+  height: 240px;
+  margin-left: 8px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+`;
+
+const PhotoWrapper = styled.img`
+  width: 160px;
+  height: 190px;
+`;
+
+const DetailWrapper = styled.div`
+  width: ${(props) => props.width};
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductDetail = styled.div`
+  margin-top: ${(props) => props.margintop || "0px"};
+  margin-bottom: ${(props) => props.marginbottom || "10px"};
+  color: ${(props) => props.color || "black"};
+  font-size: ${(props) => props.fontSize || "24px"};
+`;
+
+const ReviewButton = styled.button`
+  width: 240px;
+  height: 50px;
+  margin: 5px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  border: 0px;
+`;
